ea08fc4a0be1f468620ca8a328405ff602f39f3c
Dagmara Lewandowska <vanila.dl@gmail.com>
2018-01-24 23:08:22 +0100

    add scrollreveal plugin

---
+++ script/app.js
@@ -0,0 +1,6 @@
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.reveal', { duration: 700, delay: 100 });
+sr.reveal('.sec__contact--header', { duration: 500, origin: 'top' });
+sr.reveal('.sec__contact--tel', { duration: 500, origin: 'top' });
+sr.reveal('.sec__contact--email', { duration: 500, origin: 'top' });
