b827fc278121dedc85b66045affbce019995e8ae
Dagmara Lewandowska <vanila.dl@gmail.com>
2018-01-26 12:22:56 +0100

    RWD nav, main hero, flexbox-projects

--- script/scrollReveal.js
+++ script/scrollReveal.js
@@ -1,6 +1,7 @@
 
 window.sr = ScrollReveal({ reset: true });
 sr.reveal('.reveal', { duration: 700, delay: 100 });
+sr.reveal('.main__box__nav', { duration: 700});
 sr.reveal('.sec__contact--header', { duration: 500, origin: 'top' });
 sr.reveal('.sec__contact--tel', { duration: 500, origin: 'top' });
 sr.reveal('.sec__contact--email', { duration: 500, origin: 'top' });
